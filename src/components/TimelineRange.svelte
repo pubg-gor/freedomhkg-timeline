50a06c63955a69cba523e6e045a4c58ec12f7256
chauchakching <chauchakching@gmail.com>
2019-08-22 22:04:37 +0800

    feat: add calendar icon above date range

--- src/components/TimelineRange.svelte
+++ src/components/TimelineRange.svelte
@@ -1,6 +1,8 @@
 <script>
   import * as R from 'ramda'
   import {DateTime} from 'luxon'
+  import Fa from 'svelte-fa'
+  import { faCalendar } from '@fortawesome/free-solid-svg-icons/faCalendar'
   import {debounce} from '../utils/functionUtil'
   import Range from './Range/index.svelte'
   import {events, beforeDate} from '../stores'
@@ -29,4 +31,16 @@
   }
 </script>
 
+<style>
+  top-icon-wrapper {
+    position: relative;
+    left: -8px;
+    top: -5px;
+    font-size: 18px;
+  }
+</style>
+
+<top-icon-wrapper>
+  <Fa icon={faCalendar} />
+</top-icon-wrapper>
 <Range bind:ratio={ratio} valueText={selectedDay} />
