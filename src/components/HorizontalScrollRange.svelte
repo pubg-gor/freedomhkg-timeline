4fa2632c6ef8368cad2d7528c790186272c49ecc
chauchakching <chauchakching@gmail.com>
2019-09-05 22:18:16 +0800

    feat: enhence mobile bottom date range bar UI

--- src/components/HorizontalScrollRange.svelte
+++ src/components/HorizontalScrollRange.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount } from 'svelte'
   import * as R from 'ramda'
   import { DateTime } from '../utils/luxon'
   import { beforeDate, dateRangeData, loading } from '../stores'
@@ -7,13 +8,21 @@
   let itemDoms = []
   let selectedDay
 
-  function onScroll() {
+  function updateSelectedDay() {
     selectedDay = R.pipe(
       R.map(ele => ele.getBoundingClientRect()),
-      R.findIndex(({ x, width }) => x > -width),
+      R.findIndex(({ x, width }) => x >= window.innerWidth / 2 - width && x < window.innerWidth / 2),
       i => $dateRangeData.monthDays[i]
     )(itemDoms)
   }
+
+  function onScroll() {
+    updateSelectedDay()
+  }
+
+  onMount(() => {
+    updateSelectedDay()
+  })
 
   $: updateBeforeDate = debounce(350, beforeDate.set)()
   $: if (typeof window !== 'undefined' && !R.isNil(selectedDay)) {
@@ -42,35 +51,51 @@
     align-items: center;
     justify-content: center;
     flex: 1 0 auto;
+    padding-left: calc(50vw - var(--range-item-width) / 2);
     padding-right: calc(100vw - var(--range-item-width));
-    height: 44px;
+    height: var(--bar-height);
   }
   range-item {
     width: var(--range-item-width);
     border-left: solid 1px #efefef;
     display: flex;
     align-items: center;
-    justify-content: flex-end;
-    padding-right: 16px;
+    justify-content: center;
+    opacity: 0.2;
+  }
+  range-item.selected {
+    border: solid 1px #cccccc;
+    opacity: 1;
   }
   loading-placeholder {
     width: 100vw;
     text-align: center;
+  }
+  down-pointer {
+    position: fixed;
+    left: 50vw;
+    bottom: calc(var(--bar-height) - 8px);
+		border-left: 3px solid transparent;
+		border-right: 3px solid transparent;
+
+		border-top: 6px solid #f00;
   }
 </style>
 
 <horizontal-scroll-range 
   on:scroll={onScroll}
   style="
-    --range-item-width: 70px;
+    --bar-height: 44px;
+    --range-item-width: 90px;
   "
 >
+  <down-pointer />
   <inner-bar>
-    {#if $loading}
+    {#if ($loading && false)}
       <loading-placeholder>Loading...</loading-placeholder>
     {:else}
       {#each $dateRangeData.monthDays as monthDay, index}
-        <range-item bind:this={itemDoms[index]}>{monthDay}</range-item>
+        <range-item bind:this={itemDoms[index]} class:selected={selectedDay === monthDay}>{monthDay}</range-item>
       {/each}
     {/if}
   </inner-bar>
